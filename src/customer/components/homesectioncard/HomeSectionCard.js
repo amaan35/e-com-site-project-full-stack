5efe792b237cc64b1271156cf7ddeb507bca8f44
amaan35 <amaanlm35@gmail.com>
2023-12-19 06:17:59 +0530

    Creating data folder and adding data of few products, modifying HomeSectionCard component to display dynamic data, modifying HomeSectionCarousel component to slide carousel at the click of buttons

--- src/customer/components/homesectioncard/HomeSectionCard.js
+++ src/customer/components/homesectioncard/HomeSectionCard.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const HomeSectionCard = () => {
+const HomeSectionCard = ({product}) => {
   return (
     <div style={{boxShadow:'0px 0px 5px 0px'}} className='cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-xl overflow-hidden w-[15rem] mx-3 border border-black'>
         <div className='h-[13rem] w-[10rem]'>
-           <img className='object-cover object-top w-full h-full' src='https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/e/i/h/xxl-men-regular-slim-fit-solid-button-down-collar-formal-shirt-original-imaggvvnywmgthyq.jpeg?q=70'/> 
+           <img className='object-cover object-top w-full h-full' src={product.imageUrl}/> 
         </div>
         <div className='p-4'>
-            <h3 className='text-lg font-medium text-gray-900'>NoFilter</h3>
-            <p className='mt-2 text-sm text-gray-500'>Men Solid Pure Cotton Straight Kurta</p>
+            <h3 className='text-lg font-medium text-gray-900'>{product.title}</h3>
+            <p className='mt-2 text-sm text-gray-500'>{product.brand}</p>
         </div>
     </div>
   )
--- src/customer/components/homesectioncarousel/HomeSectionCarousel.js
+++ src/customer/components/homesectioncarousel/HomeSectionCarousel.js
@@ -1,54 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import AliceCarousel from "react-alice-carousel";
 import HomeSectionCard from "../homesectioncard/HomeSectionCard";
 import { Button } from "@mui/material";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import { mens_kurta } from "../../../data/mens_kurta";
 
 const HomeSectionCarousel = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
   const responsive = {
     0: { items: 1 },
     720: { items: 3 },
-    1024: { items: 5.2 },
+    1024: { items: 5 },
   };
-  const items = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item) => {
-    return <HomeSectionCard />;
-  });
+
+  const slidePrev = () => {
+    setActiveIndex(activeIndex - 1);
+  };
+  const slideNext = () => {
+    setActiveIndex(activeIndex + 1);
+  };
+
+  const syncActiveIndex = ({ item }) => setActiveIndex(item);
+
+  const items = mens_kurta
+    .slice(activeIndex, activeIndex + 5)
+    .map((item, index) => <HomeSectionCard key={index} product={item} />);
+
   return (
     <div className="px-4 lg:px-8">
       <div className="relative p-5 bg-gray-300 border rounded-lg">
         <AliceCarousel
           items={items}
           disableButtonsControls
-          infinite
           responsive={responsive}
           disableDotsControls
+          onSlideChanged={syncActiveIndex}
+          activeIndex={activeIndex}
         />
-        <Button
-          variant="contained"
-          className="z-50"
-          sx={{
-            position: "absolute",
-            top: "8rem",
-            right: "0rem",
-            transform: "translateX(50%) rotate(90deg)",
-          }}
-          aria-label="next"
-        >
-          <KeyboardArrowLeftIcon sx={{ transform: "rotate(90deg)" }} />
-        </Button>
-        <Button
-          variant="contained"
-          className="z-50"
-          sx={{
-            position: "absolute",
-            top: "8rem",
-            left: "0rem",
-            transform: "translateX(-50%) rotate(-90deg)",
-          }}
-          aria-label="next"
-        >
-          <KeyboardArrowLeftIcon sx={{ transform: "rotate(90deg)" }} />
-        </Button>
+
+        {activeIndex < items.length-1 && (
+          <Button
+            variant="contained"
+            className="z-50"
+            onClick={slideNext}
+            sx={{
+              position: "absolute",
+              top: "8rem",
+              right: "0rem",
+              transform: "translateX(50%) rotate(90deg)",
+            }}
+            aria-label="next"
+          >
+            <KeyboardArrowLeftIcon sx={{ transform: "rotate(90deg)" }} />
+          </Button>
+        )}
+
+        {activeIndex !== 0 && (
+          <Button
+            variant="contained"
+            className="z-50"
+            onClick={slidePrev}
+            sx={{
+              position: "absolute",
+              top: "8rem",
+              left: "0rem",
+              transform: "translateX(-50%) rotate(-90deg)",
+            }}
+            aria-label="next"
+          >
+            <KeyboardArrowLeftIcon sx={{ transform: "rotate(90deg)" }} />
+          </Button>
+        )}
       </div>
     </div>
   );
